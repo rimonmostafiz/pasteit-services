ba285855e75b3b16c8189079871b06c3d2f2091e
Rimon Mostafiz <rimonmostafiz@gmail.com>
2021-09-02 11:20:52 -0500

    Initial Commit

---
+++ src/main/java/com/mpp/gryffindor/pasteit/PasteItApplication.java
@@ -0,0 +1,13 @@
+package com.mpp.gryffindor.pasteit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasteItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mpp/gryffindor/pasteit/PasteItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpp.gryffindor.pasteit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
