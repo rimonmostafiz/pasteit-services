727e418b892f14d9ec39a2d4bddc783cf36c7199
Abdiwjilo <abdiwjilo@gmail.com>
2021-09-04 21:53:57 -0500

    Bugfix on FeedbackModel Id String to Long

--- src/main/java/com/miu/pasteit/model/dto/FeedbackModel.java
+++ src/main/java/com/miu/pasteit/model/dto/FeedbackModel.java
@@ -1,15 +1,11 @@
 package com.miu.pasteit.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.miu.pasteit.model.entity.common.Language;
-import com.miu.pasteit.model.entity.common.PasteStatus;
-import com.miu.pasteit.model.entity.db.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 /**
  * @author Abdi Wako Jilo
@@ -19,7 +15,7 @@ import java.util.List;
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class FeedbackModel {
-    private String id;
+    private Long id;
     private String comment;
     private LocalDateTime dateTime;
     private UserModel user;
