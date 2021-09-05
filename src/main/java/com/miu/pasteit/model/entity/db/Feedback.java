8bc9c11409cf09f165699f8a1b99a4a60145607b
Abdiwjilo <abdiwjilo@gmail.com>
2021-09-04 21:25:18 -0500

    Add on Feedback

--- src/main/java/com/miu/pasteit/model/entity/db/Feedback.java
+++ src/main/java/com/miu/pasteit/model/entity/db/Feedback.java
@@ -1,9 +1,13 @@
 package com.miu.pasteit.model.entity.db;
 
+import com.miu.pasteit.model.entity.common.EntityCommon;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import java.util.List;
+
+import javax.persistence.OneToMany;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /**
  * @author Abdi Wako Jilo
@@ -11,13 +15,21 @@ import java.util.List;
 @Data
 @ToString
 @NoArgsConstructor
-@AllArgsConstructor
-@Document("PASTE")
-public class Feedback {
+@AllArgsConstructor(staticName = "of")
+@Document("FEEDBACK")
+public class Feedback extends EntityCommon {
 
     @Id
-    private int id;
-    private List<String> comment;
-    private int like;
+    private String id;
+    private String comment;
+    private LocalDateTime dateTime;
 
+    @OneToMany
+    private User user;
+
+    public Feedback(String comment, LocalDateTime dateTime, User user) {
+        this.comment = comment;
+        this.dateTime = dateTime;
+        this.user = user;
+    }
 }
