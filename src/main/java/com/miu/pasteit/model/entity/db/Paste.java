4bad2db2ae42b0fcbb9d605c78e5105650b7e8c8
Abdiwjilo <abdiwjilo@gmail.com>
2021-09-04 21:25:56 -0500

    Add association on Feedback to Paste

--- src/main/java/com/miu/pasteit/model/entity/db/Paste.java
+++ src/main/java/com/miu/pasteit/model/entity/db/Paste.java
@@ -7,12 +7,16 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.persistence.CascadeType;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.OneToMany;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * @author Rimon Mostafiz
+ * @author Rimon Mostafiz and Abdi Wako Jilo
  */
 
 @Data
@@ -51,4 +55,14 @@ public class Paste extends EntityCommon {
     private LocalDateTime pasteDateTime;
 
     private String share;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Feedback> feedback = new ArrayList<>();
+
+    private int likes;
+
+    public void addFeedBack(Feedback feedback){
+        this.feedback.add(feedback);
+    }
+
 }
