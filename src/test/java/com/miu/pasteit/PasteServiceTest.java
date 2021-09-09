489584f18e3cdd2b432f83e5f9c13226d6a5ce76
eyobmbt <eyobmbt@gmail.com>
2021-09-09 08:31:28 -0500

    Update PasteServiceTest.java

--- src/test/java/com/miu/pasteit/PasteServiceTest.java
+++ src/test/java/com/miu/pasteit/PasteServiceTest.java
@@ -1,49 +1,69 @@
-package com.miu.pasteit.service.paste;
+package com.miu.pasteit;
 
 import com.miu.pasteit.model.dto.PasteModel;
-import com.miu.pasteit.model.dto.UserModel;
-import com.miu.pasteit.model.entity.activity.ActivityPaste;
-import com.miu.pasteit.model.entity.common.Language;
 import com.miu.pasteit.model.entity.common.PasteStatus;
+import com.miu.pasteit.model.entity.db.nosql.Paste;
+import com.miu.pasteit.model.entity.db.sql.User;
+import com.miu.pasteit.model.mapper.PasteMapper;
 import com.miu.pasteit.model.request.PasteCreateRequest;
-import com.miu.pasteit.repository.PasteRepository;
+import com.miu.pasteit.repository.mongo.PasteRepository;
+import com.miu.pasteit.service.paste.PasteService;
+import com.miu.pasteit.service.user.UserService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
-import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class PasteServiceTest {
+public class PasteServiceTest {
+
 
     @Autowired
+    private UserService userService;
+    @Autowired
     private PasteService pasteService;
+    @Autowired
+    private PasteRepository pasteRepository;
 
-    @MockBean
-    private ActivityPaste pasteModel;
+    @Autowired
+    PasteCreateRequest pasteCreateRequest;
+
 
     @Test
     void createPaste() {
+        pasteCreateRequest=new
+                PasteCreateRequest("int var=5;","java code","java code description","PUBLIC","Java","C:\\");
+        User user=userService.getUserByUsername("eyob");
+        Paste paste=PasteMapper.createRequestToEntity(pasteCreateRequest,"eyob",user);
+        Paste savedPaste=pasteRepository.save(paste);
 
+        assertEquals(paste.getId(),savedPaste.getId());
     }
 
     @Test
-    void getPaste() {
-        LocalDateTime now=LocalDateTime.now();
-        LocalDateTime expire=now.plusDays(2);
-        PasteModel paste= new PasteModel();
-        paste.setId("123");
-        paste.setTitle("Java Code");
-        paste.setUrl("http://lcoalhost");
-        paste.setContent("public static int=10;");
-        paste.setPasteDateTime(now);
-        paste.setExpiryDateTime(expire);
-        paste.setPasteUser(new UserModel());
-        paste.setShare("public");
+    void getAllPasteByStatus(PasteStatus aPublic) {
+        List<PasteCreateRequest> listPastesByStatus= new LinkedList<>();
+        User user=userService.getUserByUsername("eyob");
+        listPastesByStatus.add(new
+                PasteCreateRequest("int var=10;","java code","java code description","PUBLIC","Java","C:\\"));
+        listPastesByStatus.add(new
+                PasteCreateRequest("int var=12;","java Code","Java code description","PRIVATE","Java","C:\\"));
+        listPastesByStatus.add(new
+                PasteCreateRequest("int var=15;","C++ code","C++ code description","PUBLIC","Java","C:\\"));
 
-        Mockito.when(pasteModel.getId()).thenReturn(paste.getId());
-        assertEquals(pasteService.getPaste("123").getId(),paste.getId());
+
+        for (PasteCreateRequest pm:listPastesByStatus)
+        {
+            Paste paste=PasteMapper.createRequestToEntity(pasteCreateRequest,"eyob",user);
+            Paste savedPaste=pasteRepository.save(paste);
+
+        }
+        List<PasteModel> listofPastes=pasteService.getAllPasteByStatus(PasteStatus.PUBLIC);
+        assertEquals(listofPastes.get(0).getStatus(),listPastesByStatus.get(0).getStatus());
+        assertFalse(listofPastes.get(0).getStatus().equals(listPastesByStatus.get(1).getStatus()));
+        assertEquals(listofPastes.get(0).getStatus(),listPastesByStatus.get(1).getStatus());
+
     }
 }
